b81f1ebf71757f594d02d51c8d067788248cd0c4
aznamle <namle1998@hotmail.com>
2022-04-11 16:18:31 -0700

    layout complete

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,23 +6,28 @@ import Footer from './Footer'
 
 const Layout = ({ children }) => {
   return (
-    <div className='flex flex-row h-full'>
+    <div className='flex h-screen'>
         <Head>
             <title>Chris Photography</title>
         </Head>
-        <div >
-            <div className='relative h-screen flex w-72 overflow-y-auto'>
-                <Header />
+        <div className='flex-1 flex flex-col overflow-hidden'>
+            <div className='flex h-full'>
+                <nav className='flex w-1/4 h-full'>
+                    <div className='w-full flex mx-auto py-4'>
+                        <div className='w-full h-full flex items-center justify-center text-gray-900 text-xl '>
+                            <Header />
+                        </div>
+                    </div>
+                </nav>
+                <main className='  w-full overflow-x-hidden overflow-y-auto'>
+                    <div className='flex  mx-auto p-2'>
+                        {children}
+                    </div>
+                    <footer className='flex mx-auto items-center justify-center h-24'>
+                        <Footer />
+                    </footer>
+                </main>
             </div>
-        </div>
-        <div className='max-w-full h-full flex relative overflow-y-auto'>
-            <main>
-                {children}
-            </main>
-
-        <footer>
-            <Footer />
-        </footer>
         </div>
     </div>
   )

b9ed4a7d9843d8610cd870a5fd1663dbb8f66cdf
aznamle <namle1998@hotmail.com>
2022-04-13 18:42:42 -0700

    mobile nav

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -25,7 +25,7 @@ const Gallery = ({ slice }) => {
 
                     {slice.items.map((item, i) => (
 
-                        <div key={i} className="flex relative overflow-hidden lg:w-1/2 p-2">
+                        <div key={i} className="flex relative overflow-hidden lg:w-1/3 p-2">
                                 <Image src={item.image.url} width={item.image.dimensions.width} height={item.image.dimensions.height} alt={item.image.alt}
                                 className='w-full object-cover h-full object-center block transform duration-300 ease-in-out hover:scale-105'
                                 />
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { PrismicLink, PrismicRichText } from "@prismicio/react"
 import Link from 'next/link';
 import Image from "next/image";
@@ -5,10 +6,12 @@ import { AiFillInstagram } from 'react-icons/ai'
 
 const Header = ({ menu }) => {
 
+  const [isExpanded, toggleExpansion] = useState(false)
+  const closeMobileMenu = () => toggleExpansion(false)
 
   return (
     <div className='flex'>
-      <div className='space-y-8'>
+      <div className='hidden md:block space-y-8'>
 
         <div className="flex space-x-2 items-center">
           <div className=''>
@@ -36,6 +39,24 @@ const Header = ({ menu }) => {
             ))}
           </ul>
         </div>
+      </div>
+
+      <div className='block md:hidden'>
+        <div className='flex items-center justify-between bg-white flex-wrap p-6 shadow-md top-0 fixed inset-x-0 z-40'>
+        <div>
+        <Link href='/'>
+              <a className='text-2xl tracking-wider'>
+                @ChrisForonda
+              </a>
+            </Link>
+        </div>
+        <div className="block lg:hidden">
+          <button onClick={() => toggleExpansion(!isExpanded)} className={`flex items-center px-3 py-3 rounded hover:text-gray-200 hover:border-black transition ease-in-out duration-500`}>
+            <svg className="fill-current h-5 w-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" ><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+          </button>
+        </div>
+        </div>
+
       </div>
         
 
